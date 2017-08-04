397c09392531dca4a34c3e110184816cb77ab3e6
sungpil <kanchi@sundaytoz.com>
2017-08-04 14:55:48 +0900

    init

---
+++ anison/server/Chat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using socket.io;
+
+namespace anison.server {
+	
+	public class Chat : MonoBehaviour {
+
+		const string URL_CHAT = "http://anison.chat.stzapp.net";
+
+		static Socket socket;
+
+		public static Chat Connect(string roomId) {
+			Chat instance = new Chat ();
+			string serverUrl = URL_CHAT + "?roomId=" + roomId;
+			socket = Socket.Connect (serverUrl);
+			return instance;
+		}
+
+		public void Message (string message) {
+			socket.Emit("message",message);
+		}
+
+		public void On(string eventName, Action callback) {
+			if (null != socket) {
+				socket.On(eventName, callback);
+			}
+		}
+
+		public void On(string eventName, Action<string> callback) {
+			if (null != socket) {
+				socket.On(eventName, callback);
+			}
+		}
+	}
+}
+
---
+++ anison/server/Room.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace anison.server {
+	
+	[System.Serializable]
+	public class UserStatus {
+		public string status;
+		public string roomId;
+		override public string ToString() {
+			return "status=" + status + ", roomId=" + roomId;
+		}
+	}
+
+	public class Room : MonoBehaviour {
+
+		const string URL_ROOM = "http://anison.room.stzapp.net";
+
+		static Room mInstance;
+
+		public static Room Instance
+		{
+			get
+			{
+				if (mInstance == null){
+					GameObject go = new GameObject();
+					mInstance = go.AddComponent<Room>();
+				}
+				return mInstance;
+			}
+		}
+
+		public void GetUserStatus (string userId, Action<UserStatus> callback) {
+			string requestUrl = URL_ROOM + "?id=" + userId;
+			StartCoroutine(Reqeust(requestUrl, callback));
+		}
+
+		IEnumerator Reqeust(string requestUrl, Action<UserStatus> callback) {
+			UnityWebRequest www = UnityWebRequest.Get(requestUrl);
+			yield return www.Send();
+
+			if(www.isError) {
+				Debug.Log(www.error);
+			}
+
+			else {
+				Debug.Log(www.downloadHandler.text);
+				UserStatus userStatus = JsonUtility.FromJson<UserStatus> (www.downloadHandler.text);
+				if (null != callback) {
+					callback (userStatus);
+				}
+			}
+		}
+	}
+}
+
